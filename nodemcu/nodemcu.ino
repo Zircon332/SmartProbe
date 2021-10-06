f518ca735afbc23f114fa4a26f42c413fd0479d8
ZeeHatcher <zee.hatcher010@gmail.com>
2021-10-06 15:15:25 +0800

    extract temperature sensor data, rename and cleanup config file

--- nodemcu/nodemcu.ino
+++ nodemcu/nodemcu.ino
@@ -1,17 +1,22 @@
 #include <Arduino.h>
+#include <DallasTemperature.h>
+#include <OneWire.h>
+#include <SPI.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <WiFi.h>
 
-#include <SPI.h>
-
-#include "NodemcuConfig.h"
+#include "Config.h"
 
 #define WIFI_TIMEOUT_MS 20000
 #define MOISTURE_PIN 4
+#define TEMPERATURE_PIN 2
 
 IPAddress server(address[0], address[1], address[2], address[3]);
 WiFiClient client;
+
+OneWire oneWire(TEMPERATURE_PIN);
+DallasTemperature sensorTemperature(&oneWire);
 
 void connectToWiFi() {
   Serial.print("Connecting to WiFi");
@@ -44,23 +49,30 @@ void setup() {
   Serial.begin(9600);
 
   pinMode(MOISTURE_PIN, INPUT);
+  sensorTemperature.begin();
 }
 
 void loop() {
+  // Read sensor values
   int moisture = analogRead(MOISTURE_PIN);
+  sensorTemperature.requestTemperatures();
+  
   Serial.print("Moisture: ");
   Serial.println(moisture);
+
+  Serial.print("Temperature: ");
+  Serial.println(sensorTemperature.getTempCByIndex(0));
   
-  if (WiFi.status() == WL_CONNECTED && client.connected()) {
-    //Do something
-    Serial.println("Connected");
-  } else if (WiFi.status() != WL_CONNECTED) {
-    // (Re)Connect to WiFi network
-    connectToWiFi();
-  } else if (!client.connected()) {
-    // (Re)Connect to server
-    client.stop();
-    connectToServer();
-    delay(1000);
-  }
+//  if (WiFi.status() == WL_CONNECTED && client.connected()) {
+//    //Do something
+//    Serial.println("Connected");
+//  } else if (WiFi.status() != WL_CONNECTED) {
+//    // (Re)Connect to WiFi network
+//    connectToWiFi();
+//  } else if (!client.connected()) {
+//    // (Re)Connect to server
+//    client.stop();
+//    connectToServer();
+//    delay(1000);
+//  }
 }
