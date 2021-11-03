2e71705cde84be959c0841a74d91df0881fcb320
ZeeHatcher <zee.hatcher010@gmail.com>
2021-11-03 19:21:54 +0800

    display images from s3

--- web_app/smartprobe_web/static/js/helper.js
+++ web_app/smartprobe_web/static/js/helper.js
@@ -1,4 +1,11 @@
+const S3_ENDPOINT = "https://smartprobe.s3.ap-southeast-1.amazonaws.com/";
+
 function setIntervalExec(callback, timeout = 1000) {
   callback();
   setInterval(callback, timeout);
+}
+
+function refreshImage(img, url) {
+  let timestamp = new Date().getTime();
+  img.attr("src", `${url}?t=${timestamp}`);
 }
